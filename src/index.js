ee66bfa72fa1bfe7166aba75c2f01b2d50aa3dd3
wolsos-v <mamaev757@gmail.com>
2022-02-22 23:47:40 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) {
+    return false;
+  }
+
+  const trueStartSet = new Set();
+  let stack = [];
+
+  const config = bracketsConfig.reduce((cfg, pair) => {
+    trueStartSet.add(pair[0]);
+    cfg[pair[1]] = pair[0];
+
+    return cfg;
+  }, {});
+
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    if (stack.length && stack[stack.length - 1] === config[current]) {
+      stack.pop();
+    } else if (trueStartSet.has(current)) {
+      stack.push(current);
+    } else {
+      return false;
+    }
+  }
+
+  return !stack.length;
 }
